7df31db68dd2c4683e514b3d0d17eb6ec3b94234
Lee-kai Wang <free.leekai@gmail.com>
2015-05-14 17:44:22 +0800

    Initial Commit

---
+++ PoemTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoemTester
+//
+//  Created by Lee-kai Wang on 5/14/15.
+//  Copyright (c) 2015 Lee-kai Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
